9e3931a1d3d6f7a60c68132e8b64afd1c52db3e4
Gabriela Mejias <gmejiasg89@gmail.com>
2022-06-25 12:00:54 -0300

    Creando primeros componentes
    se convirtio el tema html en 6 componente:
    - navbar
    - portada
    - acercade
    - educacion
    - presentacion
    - experiencia
    
    close #1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeaderComponent } from './header/header.component';
+import { AcercadeComponent } from './acercade/acercade.component';
+import { ExperienciaComponent } from './experiencia/experiencia.component';
+import { EducacionComponent } from './educacion/educacion.component';
+import { PresentacionComponent } from './presentacion/presentacion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AcercadeComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    NavbarComponent,
+    PresentacionComponent
   ],
   imports: [
     BrowserModule,
